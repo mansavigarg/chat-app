2d4589c4617dbf0b8ca6c15fbe5c6a3b0f372731
mansavigarg <mansavigarg9919@gmail.com>
2026-06-10 10:24:01 +0530

    setup

--- BE/dist/index.js
+++
@@ -1,8 +0,0 @@
-import { WebSocketServer } from "ws";
-const wss = new WebSocketServer({ port: 8080 });
-let userCount = 0;
-wss.on("connection", (socket) => {
-    userCount++;
-    console.log("user connected --- #" + userCount);
-});
-//# sourceMappingURL=index.js.map
